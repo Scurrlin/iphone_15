062a8d55c0a7ef72d2b169698cedd0a6fadf2384
Sean Currlin <sean.currlin@gmail.com>
2024-09-03 09:12:33 -0400

    layout fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+import { Tilt}  from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -40,14 +40,18 @@ const About = () => {
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        What distinguishes me is my expertise in developing and optimizing web applications, enhancing data pipelines, and implementing analytics to generate actionable insights. I have a demonstrated proficiency in software development, with a proven track record of delivering high-quality solutions from ideation to deployment. I possess a deep understanding of full-stack development, particularly in Python, JavaScript, React.js, and database management. My polished communication skills enable me to effectively articulate complex technical strategies.
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
