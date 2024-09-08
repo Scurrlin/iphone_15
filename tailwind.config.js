56a7c81d3f3b427c061ce58f771eb02ef67b0796
Sean Currlin <sean.currlin@gmail.com>
2024-09-08 10:17:38 -0400

    decrease font size in footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontSize: {
+        xxs: '0.625rem'
+      },
       colors: {
         blue: "#2997FF",
         gray: {
