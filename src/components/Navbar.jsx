589c2cf790a266ba6137c5da46854c4bd67b16c9
Sean Currlin <sean.currlin@gmail.com>
2025-11-04 10:34:31 -0500

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center z-10 relative">
       <nav className="flex w-full screen-max-width">
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
